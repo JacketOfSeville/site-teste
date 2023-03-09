88b3a7634f9e579cc16b0e7dc3353bbf9074aac4
Jacket <jacketfollowme@gmail.com>
2023-03-09 15:37:50 -0300

    Mexendo com Datas no JS e colocando mais cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,34 @@
 console.log("hOi!")
 
-// Botão Google
-document.getElementById("botGoogle").onclick = function () {
-    location.href = "https://google.com.br";
-} 
+// // Botão Google
+// document.getElementById("botGoogle").onclick = function () {
+//     location.href = "https://google.com.br";
+// } 
 
-// Botão Youtube
-document.getElementById("botYoutube").onclick = function () {
-    location.href = "https://youtube.com.br";
-} 
+// // Botão Youtube
+// document.getElementById("botYoutube").onclick = function () {
+//     location.href = "https://youtube.com.br";
+// } 
 
-// Botão Reddit
-document.getElementById("botReddit").onclick = function () {
-    location.href = "https://reddit.com";
-} 
+// // Botão Reddit
+// document.getElementById("botReddit").onclick = function () {
+//     location.href = "https://reddit.com";
+// } 
 
-// Botão GitHub
-document.getElementById("botGithub").onclick = function () {
-    location.href = "https://github.com";
-} 
+// // Botão GitHub
+// document.getElementById("botGithub").onclick = function () {
+//     location.href = "https://github.com";
+// } 
+
+function dataHoje () {
+    const d = new Date().toLocaleDateString();
+    return d;
+}
+
+// let dia = d.getDay();
+// let mes = d.getMonth();
+// let ano = d.getYear();
+
+// console.log(`${dia}/${mes}/${ano}`);
+
+console.log(d);
