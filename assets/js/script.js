ab9ae5392dc5437ce36e356302b44faa773841e1
Jacket <jacketfollowme@gmail.com>
2023-03-09 12:25:24 -0300

    Mexendo com botões

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 console.log("hOi!")
+
+document.getElementById("botGoogle").onclick = function () {
+    location.href = "https://google.com.br";
+} 
+
+document.getElementById("botYoutube").onclick = function () {
+    location.href = "https://youtube.com.br";
+} 
