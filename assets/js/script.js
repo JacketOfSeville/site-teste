fb7febb7eebf002192a9110bf14b7a039f89c3be
Jacket <jacketfollowme@gmail.com>
2023-03-09 15:06:29 -0300

    Talvez pega o design parecido com o de um blog?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 console.log("hOi!")
 
+// Botão Google
 document.getElementById("botGoogle").onclick = function () {
     location.href = "https://google.com.br";
 } 
 
+// Botão Youtube
 document.getElementById("botYoutube").onclick = function () {
     location.href = "https://youtube.com.br";
+} 
+
+// Botão Reddit
+document.getElementById("botReddit").onclick = function () {
+    location.href = "https://reddit.com";
+} 
+
+// Botão GitHub
+document.getElementById("botGithub").onclick = function () {
+    location.href = "https://github.com";
 } 
