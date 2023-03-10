319bac1bb5a1f72e79e64c818dedd8509212eceb
Jacket <jacketfollowme@gmail.com>
2023-03-10 12:22:58 -0300

    Colocando um marcador de data na direita do site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,72 @@
 console.log("hOi!")
 
-// // Botão Google
-// document.getElementById("botGoogle").onclick = function () {
-//     location.href = "https://google.com.br";
-// } 
+// Botão Google
+document.getElementById("botGoogle").onclick = function () {
+    location.href = "https://google.com.br";
+} 
 
-// // Botão Youtube
-// document.getElementById("botYoutube").onclick = function () {
-//     location.href = "https://youtube.com.br";
-// } 
+// Botão Youtube
+document.getElementById("botYoutube").onclick = function () {
+    location.href = "https://youtube.com.br";
+} 
 
-// // Botão Reddit
-// document.getElementById("botReddit").onclick = function () {
-//     location.href = "https://reddit.com";
-// } 
+// Botão Reddit
+document.getElementById("botReddit").onclick = function () {
+    location.href = "https://reddit.com";
+} 
 
-// // Botão GitHub
-// document.getElementById("botGithub").onclick = function () {
-//     location.href = "https://github.com";
-// } 
+// Botão GitHub
+document.getElementById("botGithub").onclick = function () {
+    location.href = "https://github.com";
+} 
 
-function dataHoje () {
-    const d = new Date().toLocaleDateString();
-    return d;
+/*----------------------------------------------------------*/
+
+const data = document.getElementById("datadireita"); // Chama a área de data no HTML
+const d = new Date(); // Cria a data do presente momento
+
+let dds = d.getDay();  // Pega o dia da semana
+switch (dds) {
+    case 0:
+        dds = `Domingo`;
+        break;
+    case 1:
+        dds = `Segunda-feira`;
+        break;
+    case 2:
+        dds = `Terça-feira`;
+        break;
+    case 3:
+        dds = `Quarta-feira`;
+        break;
+    case 4:
+        dds = `Quinta-feira`;
+        break;
+    case 5:
+        dds = `Sexta-feira`;
+        break;
+    case 6:
+        dds = `Sábado`;
+        break;
+    default:
+        dds = `Dia da semana`
 }
 
-// let dia = d.getDay();
-// let mes = d.getMonth();
-// let ano = d.getYear();
+let dia = d.getDate();  // Pega o dia
+if (dia < 10) {
+    dia = "0"+dia.toString();
+}
 
-// console.log(`${dia}/${mes}/${ano}`);
+const meses = [`Janeiro`,`Fevereiro`,`Março`,
+               `Abril`,`Maio`,`Junho`,
+               `Julho`,`Agosto`,`Setembro`,
+               `Outubro`,`Novembro`,`Dezembro`] // Lista de meses
+const mesId = d.getMonth();  // Adquire o mês
+let mes = meses[mesId];   // Usa o Id do mês para resgatar o nome
 
-console.log(d);
+const ano = d.getFullYear(); // Pega o ano
+
+data.innerHTML = `<h2 style="margin-bottom: 5px">${dds}</h2> 
+                  ${dia} de ${mes} de ${ano}<br>`;
+
+console.log(dataHoje());
