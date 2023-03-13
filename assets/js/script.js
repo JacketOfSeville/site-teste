7b12f2e5acde70833ac51e26fc1973d7978f37c9
Jacket <jacketfollowme@gmail.com>
2023-03-13 08:46:07 -0300

    Otimização e remoção de coisas que julguei "não necessárias"

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,51 +22,55 @@ document.getElementById("botGithub").onclick = function () {
 
 /*----------------------------------------------------------*/
 
-const data = document.getElementById("datadireita"); // Chama a área de data no HTML
-const d = new Date(); // Cria a data do presente momento
+function DataHoje () {
+    const data = document.getElementById("datadireita"); // Chama a área de data no HTML
+    const d = new Date(); // Cria a data do presente momento
 
-let dds = d.getDay();  // Pega o dia da semana
-switch (dds) {
-    case 0:
-        dds = `Domingo`;
-        break;
-    case 1:
-        dds = `Segunda-feira`;
-        break;
-    case 2:
-        dds = `Terça-feira`;
-        break;
-    case 3:
-        dds = `Quarta-feira`;
-        break;
-    case 4:
-        dds = `Quinta-feira`;
-        break;
-    case 5:
-        dds = `Sexta-feira`;
-        break;
-    case 6:
-        dds = `Sábado`;
-        break;
-    default:
-        dds = `Dia da semana`
+    let dds = d.getDay();  // Pega o dia da semana
+    switch (dds) {
+        case 0:
+            dds = `Domingo`;
+            break;
+        case 1:
+            dds = `Segunda-feira`;
+            break;
+        case 2:
+            dds = `Terça-feira`;
+            break;
+        case 3:
+            dds = `Quarta-feira`;
+            break;
+        case 4:
+            dds = `Quinta-feira`;
+            break;
+        case 5:
+            dds = `Sexta-feira`;
+            break;
+        case 6:
+            dds = `Sábado`;
+            break;
+        default:
+            dds = `Dia da semana`
+    }
+
+    let dia = d.getDate();  // Pega o dia
+    if (dia < 10) {
+        dia = "0"+dia.toString();
+    }
+
+    const meses = [`Janeiro`,`Fevereiro`,`Março`,
+                `Abril`,`Maio`,`Junho`,
+                `Julho`,`Agosto`,`Setembro`,
+                `Outubro`,`Novembro`,`Dezembro`] // Lista de meses
+    const mesId = d.getMonth();  // Adquire o mês
+    let mes = meses[mesId];   // Usa o Id do mês para resgatar o nome
+
+    const ano = d.getFullYear(); // Pega o ano
+
+    data.innerHTML = `<h2 style="margin-bottom: 5px">${dds}</h2> 
+                    ${dia} de ${mes} de ${ano}<br>`;
 }
 
-let dia = d.getDate();  // Pega o dia
-if (dia < 10) {
-    dia = "0"+dia.toString();
-}
-
-const meses = [`Janeiro`,`Fevereiro`,`Março`,
-               `Abril`,`Maio`,`Junho`,
-               `Julho`,`Agosto`,`Setembro`,
-               `Outubro`,`Novembro`,`Dezembro`] // Lista de meses
-const mesId = d.getMonth();  // Adquire o mês
-let mes = meses[mesId];   // Usa o Id do mês para resgatar o nome
-
-const ano = d.getFullYear(); // Pega o ano
-
-data.innerHTML = `<h2 style="margin-bottom: 5px">${dds}</h2> 
-                  ${dia} de ${mes} de ${ano}<br>`;
+DataHoje();
 
 console.log(dataHoje());
